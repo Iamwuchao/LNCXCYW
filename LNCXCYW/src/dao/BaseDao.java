07a31b7006dd2de39759af346b4ba223c3135db7
root <root@ubuntu>
2016-03-28 09:19:38 +0800

    delete main.jsp

--- LNCXCYW/src/dao/BaseDao.java
+++ LNCXCYW/src/dao/BaseDao.java
@@ -1,5 +1,17 @@
 package dao;
 
-public interface BaseDao {
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
+/*
+ * 数据库操作 DAO接口，所有数据库操作均需要继承自该接口
+ */
+public interface BaseDao<T,PK> {
+	boolean save(T entity) throws Exception;
+	boolean update(T entity) throws Exception;
+	boolean delete(T entity) throws Exception;
+	boolean saveAll(Collection<T> entities);
+	List<T> findById(PK pk);
+	List<T> find(Serializable id);
 }
