b9b8ff0478180f3d7573c79458d35678100bf08e
root <root@ubuntu>
2016-03-28 10:12:33 +0800

    hhhh

--- LNCXCYW/src/dao/BaseDao.java
+++ LNCXCYW/src/dao/BaseDao.java
@@ -8,10 +8,14 @@ import java.util.List;
  * 数据库操作 DAO接口，所有数据库操作均需要继承自该接口
  */
 public interface BaseDao<T,PK> {
-	boolean save(T entity) throws Exception;
+	/*
+	 * 返回函数是否执行成功 如果中间出现异常可选择向上抛出
+	 */
+	boolean save(T entity);
 	boolean update(T entity) throws Exception;
 	boolean delete(T entity) throws Exception;
 	boolean saveAll(Collection<T> entities);
+	
 	List<T> findById(PK pk);
 	List<T> find(Serializable id);
 }
