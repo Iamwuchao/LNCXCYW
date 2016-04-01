39576299704ea9b308916348e5c955b7053007a2
root <root@ubuntu>
2016-04-01 10:17:11 +0800

    change baseDao ,but this class is useless

--- LNCXCYW/src/dao/BaseDao.java
+++ LNCXCYW/src/dao/BaseDao.java
@@ -1,6 +1,6 @@
 package dao;
 
-import java.io.Serializable;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -9,12 +9,12 @@ import java.util.List;
  */
 public interface BaseDao<T,PK> {
 	/*
-	 * 返回函数是否执行成功
+	 * 返回函数是否执行成功 如果中间出现异常可选择向上抛出
 	 */
-	void setClass(Class classType);
-	void save(T entity);
+	boolean save(T entity);
 	boolean update(T entity);
 	boolean delete(T entity);
 	boolean saveAll(Collection<T> entities);
+	
 	List<T> findById(PK pk);
 }
